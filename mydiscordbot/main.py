898cd0aa163173e7aab4c709977714a2da3c662c
Daviaud Louis <louis-richard.daviaud@etu.umontpellier.fr>
2019-05-02 01:14:51 +0200

    cleanup

--- mydiscordbot/main.py
+++ mydiscordbot/main.py
@@ -18,7 +18,7 @@ from youtube_dl import YoutubeDL
 
 ytdlopts = {
     'format': 'bestaudio/best',
-    'outtmpl': 'downloads/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'outtmpl': 'music/%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'restrictfilenames': True,
     'noplaylist': True,
     'nocheckcertificate': True,
